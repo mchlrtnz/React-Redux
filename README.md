8c7228e3c1bb6d54d2adfe015ab9124faf27b11f
mchlrtnz <mchlrtnz@gmail.com>
2020-06-17 15:11:34 -0400

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # React-Redux
-Building a React and Redux app without using create-react-app tool
+
+Building a React and Redux app without using the create-react-app tool.
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>React and Redux</h1>
+      <p>
+        Building a React and Redux app without using the create-react-app tool.
+      </p>
+    </div>
+  );
+}
+
+export default App;

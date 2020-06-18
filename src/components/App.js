0dc06b75917b17f083cd692014196639ab12ea51
mchlrtnz <mchlrtnz@gmail.com>
2020-06-17 22:45:38 -0400

    Configured App with SASS/SCSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import UserContainer from "./user/container";
 
 function App() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,45 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  watch: true,
   entry: "./src/index.js",
   output: {
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js)$/,
         exclude: "/node_modules/",
         use: "babel-loader",
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.s(a|c)ss$/,
+        use: [
+          process.env.NODE_ENV !== "production"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            query: {
+              modules: true
+            },
+          },
+          "sass-loader",
+        ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
   ],
 };
